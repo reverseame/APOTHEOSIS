2517f484882d3d83ca0b432f7f32c4f2e29acd9a
Ricardo J. Rodríguez <ricardo@thanatos.local>
2024-02-17 12:32:43 +0100

    minor update for plotting results

--- plotting/plot.py
+++ plotting/plot.py
@@ -20,42 +20,36 @@ matplotlib.rcParams['text.usetex'] = True
 script_name = os.path.basename(__file__)
 PLOT_DIR = 'plots'
 output_folder = os.path.join(os.getcwd(), PLOT_DIR)
+LOG_DIR = 'logs'
 
 df = None
 X = 'MMAX'
 Y = 'MMAX0'
 Z = 'TIME'
 
+#TODO
+# Calcular correlación y covarianza entre variables?
+# https://towardsdatascience.com/how-to-measure-relationship-between-variables-d0606df27fd8
+
 def model(x1, y1):
     global X
     global Y
     global Z 
     global df_insert
+    global df
 
     df_aux = df[df[Y] == y1]
     df_aux = df_aux[df_aux[X] == x1]
     return df_aux[Z].values[0]
 
-df_csv = pd.read_csv(sys.argv[1])
-
-#plt.style.use('seaborn-whitegrid')
-
-df_csv.head()
-
-df_search = df_csv[df_csv["TYPE"] == "S"]
-df_insert = df_csv[df_csv["TYPE"] == "I"]
-
-filename = "".join(sys.argv[1].split('.')[:-1])
-
-def plot(df: pd.DataFrame, M: int, ef:int, npages: int, op: str, lims: tuple) -> (str, str):
+def plot(df: pd.DataFrame, M: int, ef:int, npages: int, op: str, lims: tuple, hexa: bool=False) -> (str, str):
     global X
     global Y
     global Z 
-    global filename 
     
     fig = plt.figure()
     threedee = fig.add_subplot(projection='3d')
-    threedee.scatter(df[X], df[Y], df[Z])
+    threedee.scatter(df[X], df[Y], df[Z],marker='*')
     threedee.set_xlabel(X)
     threedee.set_ylabel(Y)
     threedee.set_zlabel(Z)
@@ -75,24 +69,26 @@ def plot(df: pd.DataFrame, M: int, ef:int, npages: int, op: str, lims: tuple) ->
     threedee.set_zlabel(_str, rotation=90)
 
     #plt.show()
-    suffix = f"_M{M}_X{X}_Y{Y}_Z{Z}_{op}"
-    filename_aux = filename + suffix
+    filename = f"N{npages}_M{M}_X{X}_Y{Y}_Z{Z}_{op}"
     plt.title(f"{op} results for M={M}, ef={ef}, N={npages}")
-    plt.savefig(os.path.join(output_folder, filename_aux + "_scatter.pdf"), format="pdf")
-    str_scatter = filename_aux + "_scatter.pdf"
+    plt.savefig(os.path.join(output_folder, filename + "_scatter.pdf"), format="pdf")
+    str_scatter = filename + "_scatter.pdf"
     plt.clf()
 
     x = df[X]
     y = df[Y]
     z = df[Z]
 
-    from sensitivity import SensitivityAnalyzer
-    _dict = {'x1': x.values.tolist(), 'y1': y.values.tolist()}
-    sa = SensitivityAnalyzer(_dict, model)
-    df.plot.hexbin(x=X, y=Y, C=Z, gridsize=5, cmap=cm.inferno, sharex=False, xticks=xticks, yticks=yticks)
-    plt.title(f"{op} results for M={M}, ef={ef}, N={npages}")
-    plt.savefig(os.path.join(output_folder, filename_aux + "_hex.pdf"), format="pdf")
-    str_hex = filename_aux + "_hex.pdf"
+    if hexa:
+        from sensitivity import SensitivityAnalyzer
+        _dict = {'x1': x.values.tolist(), 'y1': y.values.tolist()}
+        sa = SensitivityAnalyzer(_dict, model)
+        df.plot.hexbin(x=X, y=Y, C=Z, gridsize=5, cmap=cm.inferno, sharex=False, xticks=xticks, yticks=yticks)
+        plt.title(f"{op} results for M={M}, ef={ef}, N={npages}")
+        plt.savefig(os.path.join(output_folder, filename + "_hex.pdf"), format="pdf")
+    
+    str_hex = filename + "_hex.pdf"
+    
     plt.clf()
     plt.close()
     return str_scatter, str_hex
@@ -154,70 +150,17 @@ def create_wd():
     os.mkdir(output_folder)
 
 
-try: 
-    create_wd()
-except:
-    pass
+def write_plots_latex(filename, insert, search, ncols):
+    with open(filename, "w") as f:
+        f.write("\\begin{longtable}{" + "c"*ncols + "}\n")
+        f.write(insert.replace("0.5", str(1/ncols)))
+        f.write("\\end{longtable}\n")
+        f.write("\\newpage\n")
+        f.write("\\begin{longtable}{" + "c"*ncols + "}\n")
+        f.write(search.replace("0.5", str(1/ncols)))
+        f.write("\\end{longtable}\n")
+        f.close()
 
-ef = 4
-npages = filename.split('_')[-1] # npages in the last number of filename, the first one is factor (4*factor)
-
-str_search1 = "" # scatter
-str_search2 = "" # hex
-str_insert1 = "" # scatter
-str_insert2 = "" # hex
-idx = 0
-for M in df_csv.M.unique():
-    df = df_insert[df_insert["M"] == M]
-    zmin_lim = df_insert[Z].min()
-    zmax_lim = df_insert[Z].max()
- 
-    str1, str2 = plot(df, M, ef, npages, "INSERT", (zmin_lim, zmax_lim))
-    str_insert1 += "\\includegraphics[width=0.5\\columnwidth]{" + PLOT_DIR + "/" + str1 + "}"
-    str_insert2 += "\\includegraphics[width=0.5\\columnwidth]{" + PLOT_DIR + "/" + str2 + "}"
-    
-    df = df_search[df_search["M"] == M]
-    zmin_lim = df_search[Z].min()
-    zmax_lim = df_search[Z].max()
-    
-    str1, str2 = plot(df, M, ef, npages, "SEARCH", (zmin_lim, zmax_lim))
-    str_search1 += "\\includegraphics[width=0.5\\columnwidth]{" + PLOT_DIR +"/" + str1 + "}"
-    str_search2 += "\\includegraphics[width=0.5\\columnwidth]{" + PLOT_DIR + "/" + str2 + "}"
-    idx += 1
-    if idx % 2 == 0:
-        str_insert1 += "\\\\\n"
-        str_insert2 += "\\\\\n"
-        str_search1 += "\\\\\n"
-        str_search2 += "\\\\\n"
-    else:
-        str_insert1 += " & "
-        str_insert2 += " & "
-        str_search1 += " & "
-        str_search2 += " & "
-
-    #breakpoint()
-
-#TODO
-# Calcular correlación y covarianza entre variables:
-# https://towardsdatascience.com/how-to-measure-relationship-between-variables-d0606df27fd8
-
-with open("plots.tex", "w") as f:
-    f.write("\\begin{tabular}{c c}\n")
-    f.write(str_insert1)
-    f.write("\\end{tabular}\n")
-    f.write("\\newpage\n")
-    f.write("\\begin{tabular}{c c}\n")
-    f.write(str_search1)
-    f.write("\\end{tabular}\n")
-    f.write("\\newpage\n")
-    f.write("\\begin{tabular}{c c}\n")
-    f.write(str_insert2)
-    f.write("\\end{tabular}\n")
-    f.write("\\newpage\n")
-    f.write("\\begin{tabular}{c c}\n")
-    f.write(str_search2)
-    f.write("\\end{tabular}\n")
-    f.close()
 """
 breakpoint()
 x = np.linspace(df[X].min(), df[X].max(), len(df[X].unique()))
@@ -270,3 +213,133 @@ breakpoint()
 plt.savefig("".join(sys.argv[1].split('.')[:-1]) + ".pdf", format="pdf")
 plt.clf()
 """
+
+def get_graphs(df_csv: pd.DataFrame, ef, npages,\
+                    zmin_insert, zmax_insert, zmin_search, zmax_search, hexa: bool=False):
+    global df
+    
+    df_search = df_csv[df_csv["TYPE"] == "S"]
+    df_insert = df_csv[df_csv["TYPE"] == "I"]
+
+    insert_scatter = {}
+    insert_hex = {}
+    search_scatter = {}
+    search_hex = {}
+    for M in df_csv.M.unique():
+        insert_scatter[M] = None
+        search_scatter[M] = None
+        insert_hex[M] = None
+        search_hex[M] = None
+
+        df = df_insert[df_insert["M"] == M]
+        zmin_lim = zmin_insert 
+        zmax_lim = zmax_insert
+ 
+        str1, str2 = plot(df, M, ef, npages, "INSERT", (zmin_lim, zmax_lim), hexa)
+        str_insert_scatter  = "\\includegraphics[width=0.5\\columnwidth]{" + os.path.join("../", PLOT_DIR, str1) + "}"
+        str_insert_hex      = "\\includegraphics[width=0.5\\columnwidth]{" + os.path.join("../", PLOT_DIR, str2) + "}"
+        insert_scatter[M] = str_insert_scatter
+        insert_hex[M] = str_insert_hex
+        
+        df = df_search[df_search["M"] == M]
+        zmin_lim = zmin_search
+        zmax_lim = zmax_search
+    
+        str1, str2 = plot(df, M, ef, npages, "SEARCH", (zmin_lim, zmax_lim), hexa)
+        str_search_scatter  = "\\includegraphics[width=0.5\\columnwidth]{" + os.path.join("../", PLOT_DIR, str1) + "}"
+        str_search_hex      = "\\includegraphics[width=0.5\\columnwidth]{" + os.path.join("../", PLOT_DIR, str2) + "}"
+        search_scatter[M] = str_search_scatter
+        search_hex[M] = str_search_hex
+
+    return insert_scatter, search_scatter, insert_hex, search_hex
+
+if __name__ == "__main__":
+
+    _dir = LOG_DIR
+    if len(sys.argv) > 1:
+        _dir = sys.argv[1]
+    files = [f for f in os.listdir(_dir)]
+
+    df_total = pd.DataFrame()
+    for f in files:
+        # we assume as filename: <arbitrary str>_<factor>_<ef>_<npages>.<ext>
+        filename = "".join(f.split('.')[:-1])
+        npages = filename.split('_')[-1]
+        ef = filename.split('_')[-2] 
+        
+        df_csv = pd.read_csv(os.path.join(_dir, f))
+        df_csv['ef']= ef
+        df_csv['N']= npages
+        df_total = pd.concat([df_total, df_csv])
+
+    try: 
+        create_wd()
+    except:
+        pass
+
+    # z limits
+    zmin_insert = df_total[df_total["TYPE"] == "I"][Z].min()
+    zmax_insert = df_total[df_total["TYPE"] == "I"][Z].max()
+    zmin_search = df_total[df_total["TYPE"] == "S"][Z].min()
+    zmax_search = df_total[df_total["TYPE"] == "S"][Z].max()
+
+    results = {}
+    for N in sorted([int(n) for n in df_total.N.unique()]):
+        results[N] = {}
+        results[N]["I"] = {}
+        results[N]["S"] = {}
+        results[N]["I"]["scatter"] = {}
+        results[N]["S"]["scatter"] = {} 
+        results[N]["I"]["hex"] = {}
+        results[N]["S"]["hex"] = {}
+
+        df_N = df_total[df_total["N"] == str(N)]
+        insert_scatter, search_scatter, insert_hex, search_hex = get_graphs(df_N, ef, str(N),\
+                                                zmin_insert, zmax_insert, zmin_search, zmax_search)
+        
+        for k, v in insert_scatter.items():
+            if results[N]["I"]["scatter"].get(k) is None:
+               results[N]["I"]["scatter"][k] = None 
+               results[N]["I"]["hex"][k] = None
+               results[N]["S"]["scatter"][k] = None
+               results[N]["S"]["hex"][k] = None
+
+            results[N]["I"]["scatter"][k] = v
+            results[N]["I"]["hex"][k] = insert_hex[k]
+            results[N]["S"]["scatter"][k] = search_scatter[k]
+            results[N]["S"]["hex"][k] = search_hex[k]
+
+    str_insert_scatter = ""
+    str_search_scatter = ""
+    vM = sorted([int(n) for n in df_total.M.unique()])
+    vN = sorted([int(n) for n in df_total.N.unique()])
+
+    idx = 0
+    for M in vM:
+        for N in vN:
+            str_insert_scatter += results[N]["I"]["scatter"][M] + " & "
+            str_search_scatter += results[N]["S"]["scatter"][M] + " & "
+            idx += 1
+            if idx % (len(vN)/2) == 0:
+                str_insert_scatter = str_insert_scatter[:-3] + "\\\\\n"
+                str_search_scatter = str_search_scatter[:-3] + "\\\\\n"
+
+    write_plots_latex(os.path.join("plotting", "plots.tex"), str_insert_scatter, str_search_scatter, int(len(vN)/2))
+    
+    str_insert_scatter = ""
+    str_search_scatter = ""
+    for N in vN:
+        idx = 0
+        for M in vM:
+            str_insert_scatter += results[N]["I"]["scatter"][M] + " & "
+            str_search_scatter += results[N]["S"]["scatter"][M] + " & "
+
+            idx += 1
+            if idx % (len(vM)/2) == 0:
+                str_insert_scatter = str_insert_scatter[:-3] + "\\\\\n"
+                str_search_scatter = str_search_scatter[:-3] + "\\\\\n"
+    
+    write_plots_latex(os.path.join("plotting", "plots2.tex"), str_insert_scatter, str_search_scatter, int(len(vM)/2))
+
+
+
