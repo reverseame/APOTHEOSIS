ba400f83b99acbf129374cdc49d53b7e61104ab7
Ricardo J. Rodríguez <ricardo@freya>
2024-08-01 13:03:52 +0200

    example of using Apotheosis to detect similarities in book chapter excerpts (with El Quixote)

---
+++ datalayer/node/chapbook_node.py
@@ -0,0 +1,61 @@
+#TODO docstring
+from datalayer.node.hash_node import HashNode
+from datalayer.hash_algorithm.hash_algorithm import HashAlgorithm
+from common.constants import *
+from common.errors import NodeUnsupportedAlgorithm
+
+class ChapBookHashNode(HashNode):
+    def __init__(self, id, hash_algorithm: HashAlgorithm, filename: str=None):
+        super().__init__(id, hash_algorithm)
+        self._filename = filename
+
+    def __lt__(self, other): # Hack for priority queue. TODO: not needed here?
+        return False
+
+    def get_filename(self):
+        return self._filename
+
+    def get_draw_features(self):
+        name = self.get_filename().split('.')[0]
+        _min = name.split('_')[1]
+        _max = name.split('_')[3]
+        return {"filename": { self._id: self.get_filename()},
+                "label": { self._id: f"{_min}-{_max}" },
+                "caps": { self._id: f"{_min}-{_max}" },
+                "book": { self._id: name.split('_')[1] }
+                }
+
+    def internal_serialize(self):
+        return len(self.get_filename()).to_bytes(I_SIZE, byteorder=BYTE_ORDER) + str.encode(self.get_filename())
+
+    def internal_load(cls, f):
+        blen = f.read(I_SIZE)
+        bstr = f.read(blen)
+        return blen + bstr, bstr.decode()
+
+    @classmethod
+    def create_node_from_DB(cls, db_manager, _id, hash_algorithm):
+        raise NodeUnsupportedAlgorithm # algorithm not supported
+
+    @classmethod
+    def internal_data_needs_DB(cls) -> bool:
+        return False # we have some data necessary to retrieve from the DB
+                    # to load a WinModuleHashNode from an Apotheosis file
+
+    def is_equal(self, other):
+        if type(self) != type(other):
+            return False
+        try:
+            same_file = self._filename == other._filename
+            if not same_file:
+                return False
+            if type(self._hash_algorithm) != type(other._hash_algorithm):
+                return False
+            equal = self._id == other._id
+            if not equal:
+                return False
+            
+            return True
+        except:
+            return False
+
---
+++ example_chapbooks_analysis.py
@@ -0,0 +1,81 @@
+import os
+import json
+import argparse
+import logging
+import traceback
+from apotheosis import Apotheosis
+from datetime import datetime
+from datalayer.node.chapbook_node import ChapBookHashNode
+from datalayer.hash_algorithm.tlsh_algorithm import TLSHHashAlgorithm
+from datalayer.hash_algorithm.ssdeep_algorithm import SSDEEPHashAlgorithm
+
+def configure_argparse():
+    parser = argparse.ArgumentParser(description='Configure and manage Apotheosis structures for different hash algorithms.')
+    parser.add_argument('--directory', type=str, required=True, help='Directory containing JSON files.')
+    parser.add_argument('--M', type=int, default=4, help='Number of connections of a new node when inserted.')
+    parser.add_argument('--ef', type=int, default=4, help='Controls the number of neighbors to explore during the construction and search phase of the HNSW graph')
+    parser.add_argument('--Mmax', type=int, default=8, help='Specifies the maximum number of neighbors (connections) a node can have at each layer of the hierarchy higher than zero')
+    parser.add_argument('--Mmax0', type=int, default=16, help='Specifies the maximum number of neighbors (connections) a node can have at each layer of the hierarchy at layer zero')
+    parser.add_argument('--heuristic', default=False, action='store_true', help='Enable heuristic mode')
+    parser.add_argument('--no_extend_candidates', action='store_true', help='Disable extending candidates during search')
+    parser.add_argument('--no_keep_pruned_conns', action='store_true', help='Do not keep pruned connections')
+    parser.add_argument('--beer_factor', type=float, default=0, help='Performs random walk when exploring the neighborhood')
+    parser.add_argument('--loglevel', type=str, default='ERROR', help='Set the logging level')
+    return parser
+
+def insert_node(apotheosis_instance, hash_algorithm, filename, _hash):
+    node = ChapBookHashNode(
+            id=_hash,
+            hash_algorithm=hash_algorithm,
+            filename=filename,
+        )
+    try:
+        apotheosis_instance.insert(node)
+        logging.info(f"Inserted node: {node._id} with algorithm: {hash_algorithm.__name__}")
+    except Exception as e:
+        logging.error(f"Failed to insert node: {node._id} with algorithm: {hash_algorithm.__name__} - Error: {str(e)}")
+        logging.error(traceback.format_exc())
+    
+    return apotheosis_instance
+
+def main():
+    parser = configure_argparse()
+    args = parser.parse_args()
+
+    # Configure logging
+    logging.basicConfig(level=getattr(logging, args.loglevel.upper()), format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.info("Starting Apotheosis script")
+
+    # Create instances of Apotheosis for each hash algorithm
+    logging.info("Creating Apotheosis instances")
+    apo_tlsh = Apotheosis(distance_algorithm=TLSHHashAlgorithm, M=args.M, ef=args.ef, Mmax=args.Mmax, Mmax0=args.Mmax0,
+                          heuristic=not args.heuristic, extend_candidates=not args.no_extend_candidates,
+                          keep_pruned_conns=not args.no_keep_pruned_conns, beer_factor=args.beer_factor)
+    apo_ssdeep = Apotheosis(distance_algorithm=SSDEEPHashAlgorithm, M=args.M, ef=args.ef, Mmax=args.Mmax, Mmax0=args.Mmax0,
+                            heuristic=args.heuristic, extend_candidates=not args.no_extend_candidates,
+                            keep_pruned_conns=not args.no_keep_pruned_conns, beer_factor=args.beer_factor)
+
+    directory_path = args.directory
+    logging.info(f"Processing JSON files in directory: {directory_path}")
+
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".json"):
+            file_path = os.path.join(directory_path, filename)
+
+            logging.info(f"Loading JSON file: {file_path}")
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                for filename, hashes in data.items():
+                    tlsh, ssdeep = hashes
+                    insert_node(apo_tlsh, TLSHHashAlgorithm, filename, tlsh)
+                    insert_node(apo_ssdeep, SSDEEPHashAlgorithm, filename, ssdeep)
+
+    now = datetime.now()
+    date_time = now.strftime("%Y%m%d_%H%M%S")
+
+    apo_tlsh.draw(filename="_TLSH", threshold=0)
+    apo_ssdeep.draw(filename="_ssdeep", threshold=0)
+
+if __name__ == "__main__":
+    main()
+
